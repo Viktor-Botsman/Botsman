158bf6b36a7aa9274d912a4d784f23583ae69ba3
Viktor-Botsman <57577819+Viktor-Botsman@users.noreply.github.com>
2019-11-11 22:47:18 +0200

    Update botsmansmartbot.py

--- botsmansmartbot.py
+++ botsmansmartbot.py
@@ -76,9 +76,9 @@ def send_echo(message):
 
         bot.send_message(message.chat.id, answer)
         
-        f2 = owm.daily_forecast( 'Kyiv' )
-        lst = f2.get_forecast()
-        answer = lst
+        #f2 = owm.daily_forecast( 'Kyiv' )
+        #lst = f2.get_forecast()
+        #answer = lst
         
         bot.send_message(message.chat.id, answer)
         
